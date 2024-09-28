18a26be52ea1f7106cde2f13506a319d1f4e2ad1
Bojan0611 <jbojanowski11@gmail.com>
2024-09-28 14:15:44 +0200

    post video action

--- app/routes.py
+++ app/routes.py
@@ -6,8 +6,20 @@ import os
 from app import app
 from flask import redirect, render_template, url_for
 
+from .utils.api_helper import post_video_action, result_ready
+
 app = Blueprint('app', __name__)
 
 @app.route("/", methods=["GET"])
 def home():
     return jsonify({"message": "Hello, World!"})
+
+@app.route("/api/post_video", methods=["POST"])
+def post_video():
+    result = post_video_action(request)
+    return result
+
+@app.route("/api/result_ready/<customer_id>/<session>", methods=["GET"])
+def result_ready(customer_id, session):
+    result_ready = result_ready(customer_id, session)
+    return result_ready
