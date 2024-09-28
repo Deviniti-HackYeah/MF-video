fdbf0a9addcea4f13622307ecaa06f1274e8b20b
Pawel Kiszczak <pawel.kiszczak@icloud.com>
2024-09-28 19:05:58 +0200

    simple text metrics ready

---
+++ app/utils/text_analyzer.py
@@ -0,0 +1,42 @@
+import os
+import numpy as np
+import textstat
+
+
+class TextAnalyzer:
+    def __init__(self):
+        pass
+        
+    def __str__(self):
+        return "Transcript Video using Whisper"
+        
+    
+    def gunning_fog(self, text: str) -> float:
+        """
+        The `gunning_fog` function calculates the Gunning Fog index for a given text.
+        
+        :param text: The `text` parameter in the `gunning_fog` function is expected to be a string
+        containing the text for which you want to calculate the Gunning Fog index. This text can be a
+        passage, an article, a document, or any other piece of written content for which you want to
+        :type text: str
+        :return: The `gunning_fog` readability score of the input text is being returned.
+        """
+        return textstat.gunning_fog(text)
+    
+    
+    def readibility(self, text: str) -> tuple[float, float, float]:
+        reading_ease = textstat.flesch_reading_ease(text)
+        grade = textstat.flesch_kincaid_grade(text)    
+        dale_chall = textstat.dale_chall_readability_score(text)
+        
+        return reading_ease, grade, dale_chall
+    
+    
+ta = TextAnalyzer()
+text = "W obniżeniu ulega oprocentowanie oferowanych obligacji przy jednoczesnym zachowaniu preferencji dla rynku detalicznego względem rynku hurtowego dedykowanego inwestorom instytucjonalnym."
+gf = ta.gunning_fog(text)
+print(f"GF: {gf}")
+reading_ease, grade, dale_chall = ta.readibility(text)
+print(f"Flesch reading ease: {reading_ease} | Flesch-Kincaid grade: {grade} | Dale-Chall Readibility Score: {dale_chall}")
+        
+        
--- app/utils/transcript_video.py
+++ app/utils/transcript_video.py
@@ -1,6 +1,8 @@
 from flask import jsonify
 import whisper_timestamped as whisper
 from pydub import AudioSegment
+import numpy as np
+from text_analyzer import TextAnalyzer
 # from app import whisper_model
 
 class TranscriptVideo:
@@ -11,6 +13,22 @@ class TranscriptVideo:
         return "Transcript Video using Whisper"
     
     def convert_to_mp3(self, file_path: str, output_name: str) -> bool:
+        """
+        The function `convert_to_mp3` takes a file path and output name as input, converts the file to
+        .mp3 format, and returns a boolean indicating the success of the conversion.
+        
+        :param file_path: The `file_path` parameter in the `convert_to_mp3` function is a string that
+        represents the path to the audio file that you want to convert to MP3 format. This parameter
+        should include the file name and extension (e.g., "audio.wav", "music.mp4")
+        :type file_path: str
+        :param output_name: The `output_name` parameter in the `convert_to_mp3` function is a string that
+        represents the name of the output file after the conversion process. This is the name that the
+        converted audio file will be saved as in .mp3 format
+        :type output_name: str
+        :return: The function `convert_to_mp3` is returning a boolean value. It returns `True` if the
+        conversion is successful and `False` if the conversion fails.
+        """
+        
         print(f"Converting {file_path} to .mp3... ")
         try:
             format = file_path.split('.')[-1]
@@ -24,6 +42,33 @@ class TranscriptVideo:
             return False
     
     def transcript_video(self, customer_id: str, session: str, action: str, file_path: str) -> dict:
+        """
+        The function `transcript_video` transcribes a video for a customer and session using a specified
+        action and returns the transcription result.
+        
+        :param customer_id: The `customer_id` parameter is a string that represents the unique
+        identifier of the customer for whom the video is being transcribed. It helps in identifying and
+        associating the transcripted video with the specific customer
+        :type customer_id: str
+        :param session: The `session` parameter in the `transcript_video` function is typically used to
+        identify a specific session or interaction related to the customer. It could be a unique
+        identifier for a particular video session or conversation between the customer and your system.
+        This parameter helps in tracking and organizing the transcripts based on different
+        :type session: str
+        :param action: The `action` parameter in the `transcript_video` method represents the specific
+        action or process that will be used during the transcription of the video for the given customer
+        and session. It could be a description of the transcription method being applied, such as the
+        model or algorithm used for transcription
+        :type action: str
+        :param file_path: The `file_path` parameter in the `transcript_video` function represents the
+        file path to the video file that you want to transcribe. This should be the location of the
+        video file on your system that you want to process and generate a transcript from. Make sure to
+        provide the full path to
+        :type file_path: str
+        :return: The `transcript_video` method returns a dictionary with the transcription result under
+        the key "result".
+        """
+        
         print(f"Transcripting video for customer {customer_id} and session {session} using {action}")
         
         whisper_model = whisper.load_model('large')
@@ -48,8 +93,21 @@ class TranscriptVideo:
         return output        
         # return jsonify({"status": "OK", "message": "Video is now processing. You can check if result is ready using ready_suffix"})
         
-    def word_dict(self,
-                      transcription_output: dict) -> list[dict]:
+    def word_dict(self, transcription_output: dict) -> list[dict]:        
+        """
+        This Python function processes transcription output to create a list of dictionaries containing
+        information about words and pauses.
+        
+        :param transcription_output: The `word_dict` function takes a transcription output in the form
+        of a dictionary and extracts information about the words in the transcription. It prints details
+        about each word such as the text, start time, end time, and length. If a word contains "*", it
+        prints "PAUSE" instead of the
+        :type transcription_output: dict
+        :return: The function `word_dict` returns a list of dictionaries where each dictionary
+        represents a word from the transcription output. The dictionaries contain the text of the word,
+        its start and end timestamps, and the calculated length of the word. If the word contains a "*",
+        it is represented as "[*]" in the text field of the dictionary.
+        """
         
         print(f"Transcription text: {transcription_output['result']['text']}")        
         segments = transcription_output['result']['segments']
@@ -86,10 +144,36 @@ class TranscriptVideo:
                 
                 
     def get_full_text(self, transcription_output: dict) -> str:
+        """
+        This Python function `get_full_text` extracts and returns the full text from a transcription
+        output dictionary.
+        
+        :param transcription_output: The `get_full_text` function takes two parameters: `self` and
+        `transcription_output`. The `transcription_output` parameter is expected to be a dictionary
+        containing the transcription result. The function retrieves the full text from the transcription
+        output dictionary and returns it as a string
+        :type transcription_output: dict
+        :return: The function `get_full_text` is returning the value of the 'text' key from the 'result'
+        dictionary within the `transcription_output` dictionary as a string.
+        """
+        
         return transcription_output['result']['text']
     
     
-    def text_stats(self, word_dict: dict) -> dict:
+    def total_talking_time(self, word_dict: list[dict]) -> float:
+        """
+        This function calculates the total talking time based on the start and end timestamps of words
+        in a list of dictionaries, excluding words containing "*".
+        
+        :param word_dict: The `word_dict` parameter is a list of dictionaries where each dictionary
+        represents a word spoken during a conversation. Each dictionary contains the keys 'text',
+        'start', and 'end'. The 'text' key holds the spoken word, the 'start' key holds the start time
+        of when the word
+        :type word_dict: list[dict]
+        :return: the total talking time calculated as the difference between the end time and the start
+        time of the words in the provided word dictionary.
+        """
+        
         for word in word_dict:
             if "*" not in word['text']:
                 start = word['start']
@@ -102,10 +186,87 @@ class TranscriptVideo:
                 print(f"END: {end}")
                 break
     
-        total_talking_time = end - start
-        print(total_talking_time)
-        
+        total_talking_time = end - start        
         return total_talking_time
+    
+    
+    def pause_counter(self, word_dict: list[dict]) -> tuple[float, float]:
+        """
+        This Python function calculates the total number of pauses and the total pause time based on a
+        list of dictionaries containing text and timing information.
+        
+        :param word_dict: A list of dictionaries where each dictionary represents a word with the
+        following keys:
+        :type word_dict: list[dict]
+        :return: The function `pause_counter` returns a tuple containing two values: the number of
+        pauses (`pause_count`) and the total pause time (`total_pause_time`).
+        """
+        
+        pause_count = 0
+        total_pause_time = 0
+        for word in word_dict:
+            if "*" in word['text'] and not np.isclose(word['start'], 0):
+                pause_count += 1
+                total_pause_time += word['end'] - word['start']
+                
+        return pause_count, total_pause_time
+    
+    
+    def get_full_text_from_words(self, word_dict: list[dict]) -> str:
+        full_text_list = []
+        for w in word_dict:
+            if "*" not in w['text']:
+                full_text_list.append(w['text'])
+            
+        print(full_text_list)
+        
+        full_text = " ".join(full_text_list)
+        return full_text
+    
+    
+    def word_count(self, word_dict: list[dict]) -> int:
+        return len(word_dict)
+    
+    
+    def text_stats(self, word_dict: dict) -> dict:
+        
+        # Total talking time
+        total_talking_time = self.total_talking_time(word_dict)
+        
+        # No of pauses
+        pause_count, total_pause_time = self.pause_counter(word_dict)
+        
+        # Word count
+        word_count = self.word_count(word_dict)
+        
+        # Words per second
+        words_per_second = word_count / total_talking_time
+        
+        # Readibility metrics
+        ta = TextAnalyzer()
+        full_text = self.get_full_text_from_words(word_dict)
+        gunning_fog = ta.gunning_fog(full_text)
+        reading_ease, grade, dale_chall = ta.readibility(full_text)
+                        
+        stats = {
+            'total_talking_time': total_talking_time,
+            'pause_count': pause_count,
+            'total_pause_time': total_pause_time,
+            'word_count': word_count,
+            'words_per_second': words_per_second,
+            'readibility':
+                {
+                    'gunning_fog': gunning_fog,
+                    'flesch_reading_ease': reading_ease,
+                    'flesch_kincaid_grade_level': grade,
+                    'dale_chall_readibility_score': dale_chall
+                }
+        }
+        
+        print(stats)
+        
+        
+    def run_analysis
     
     
 tv = TranscriptVideo()
